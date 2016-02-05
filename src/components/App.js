dfca4359b48e81102523ff0967c30f0522f408e8
Stéphane Derosiaux <derste@gmail.com>
2016-02-05 16:37:36 +0100

    Add redux DevTools

--- src/components/App.js
+++ src/components/App.js
@@ -9,12 +9,13 @@ import logAction from '../middlewares/logAction.js';
 import reducer from '../reducers/dailyState';
 
 import Daily from './Daily.js';
+import DevTools from './DevTools.js';
 
 const STYLE_APP = { color: 'black', fontFamily: 'Roboto', padding: 10 };
 const STYLE_TITLE = { fontSize: 30, fontFamily: 'Roboto', fontWeight: 'bold', margin: 0, padding: 10, backgroundColor: 'rgba(0,0,0,0.5)', color: '#eee' };
 
 const storeEnhancer = applyMiddleware(logState, logAction);
-const createPersistentStore = compose(persistState())(createStore);
+const createPersistentStore = compose(persistState(), DevTools.instrument())(createStore);
 const createEnhancedStore = storeEnhancer(createPersistentStore);
 const store = createEnhancedStore(reducer);
 
@@ -24,6 +25,9 @@ export default () =>
   <div style={STYLE_APP}>
     <h1 style={STYLE_TITLE}>Never forget your daily</h1>
     <Provider store={store}>
-      <Daily />
+      <div>
+        <Daily />
+        <DevTools />
+      </div>
     </Provider>
   </div>;
