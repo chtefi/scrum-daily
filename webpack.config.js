68f7b7eb8edde2e19bde5b9d436cf4341c32958e
Stéphane Derosiaux <derste@gmail.com>
2016-02-07 23:04:12 +0100

    Handle production build

--- webpack.config.js
+++ webpack.config.js
@@ -1,31 +1,31 @@
 var path = require('path');
 var webpack = require('webpack');
 
+var isProduction = (process.env.NODE_ENV === 'production');
+
+var entries = [ path.join(__dirname, 'src', 'index.js') ]
+if (!isProduction) entries.push('webpack-hot-middleware/client');
+
+var plugins = [ new webpack.DefinePlugin({ 'process.env': { 'NODE_ENV': JSON.stringify(process.env.NODE_ENV) } }), new webpack.optimize.OccurenceOrderPlugin() ];
+if (!isProduction) plugins.push(new webpack.HotModuleReplacementPlugin(), new webpack.NoErrorsPlugin());
+
+var output = {
+  path: path.join(__dirname, 'dist'),
+  filename: 'bundle.js',
+  publicPath: '/static/'
+};
+
+var loaders = [{
+  test: /\.js$/,
+  loader: 'babel',
+  include: path.join(__dirname, 'src'),
+}];
+
 module.exports = {
-  entry: [
-    'webpack-hot-middleware/client',
-    path.join(__dirname, 'src', 'index.js'),
-  ],
-  output: {
-    path: path.join(__dirname, 'dist'),
-    filename: 'bundle.js',
-    publicPath: '/static/'
-  },
+  entry: entries,
+  output: output,
   module: {
-    loaders: [{
-      test: /\.js$/,
-      loader: 'babel',
-      include: path.join(__dirname, 'src'),
-    }]
+    loaders: loaders
   },
-  plugins: [
-    new webpack.DefinePlugin({
-      'process.env': {
-        'NODE_ENV': JSON.stringify(process.env.NODE_ENV)
-      }
-    }),
-    new webpack.optimize.OccurenceOrderPlugin(),
-    new webpack.HotModuleReplacementPlugin(),
-    new webpack.NoErrorsPlugin()
-  ]
+  plugins: plugins
 };
