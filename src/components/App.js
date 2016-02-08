ed4749bd219ff338d1cbbc00c493e894cdebd19c
Stéphane Derosiaux <derste@gmail.com>
2016-02-08 22:10:55 +0100

    Externalize the store

--- src/components/App.js
+++ src/components/App.js
@@ -1,11 +1,6 @@
 import React from 'react';
-import { compose, createStore, applyMiddleware } from 'redux'
 import { Provider } from 'react-redux'
-import persistState from 'redux-localstorage';
-
-import logState from '../middlewares/logState.js';
-import logAction from '../middlewares/logAction.js';
-import reducer from '../reducers';
+import configureStore from '../reducers/store.js';
 
 import Daily from './Daily.js';
 import DevTools from './DevTools.js';
@@ -14,25 +9,12 @@ const STYLE_APP = { color: 'black', fontFamily: 'Roboto', padding: 10 };
 const STYLE_TITLE = { fontSize: 30, fontFamily: 'Roboto', fontWeight: 'bold', margin: 0, marginBottom: 10, padding: 10, backgroundColor: 'rgba(0,0,0,0.5)', color: '#eee', borderRadius: 5 };
 
 
-const DEFAULT_STATE = {
-  users: [
-    { id: 1, name: 'Oswald Cobblepot', photo: 'https://randomuser.me/api/portraits/thumb/men/92.jpg', tasks: [ { id: 1, text: 'Trying to make the state works', cdate: '2016-01-27', ddate: '2016-02-01' }, { id: 2, text: 'Trying to find which background-color fits', cdate: '2016-02-01', ddate: '2016-02-03' } ]},
-    { id: 2, name: 'Butch Gilzean', photo: 'https://randomuser.me/api/portraits/thumb/men/91.jpg', tasks: [ { id: 3, text: 'Refactor the whole project', cdate: '2016-02-01', ddate: '2016-02-03' }, { id: 4, text: 'Sending some emails', cdate: '2016-02-01', ddate: null } ]},
-    { id: 3, name: 'Fish Mooney', photo: 'https://randomuser.me/api/portraits/thumb/men/90.jpg', tasks: [ { id: 5, text: 'Do some big data', cdate: '2016-02-04', ddate: null }, { id: 6, text: 'Create a new JS framework', cdate: '2016-02-02', ddate: null } ]},
-  ],
-  weeks: {}
-};
-
-const storeEnhancer = applyMiddleware(logState, logAction);
+//const storeEnhancer = applyMiddleware(logState, logAction);
 
 // TODO(sd): Ultra ugly for now, must create distinct files to the stores, and for the root component
 // because right now, we import the whole stuff in the production bundle : it sucks.
 // https://github.com/gaearon/redux-devtools/blob/master/docs/Walkthrough.md
 const isProduction = (process.env.NODE_ENV === 'production');
-
-const createPersistentStore = (isProduction ? compose(persistState())(createStore) : compose(persistState(), DevTools.instrument())(createStore));
-const createEnhancedStore = storeEnhancer(createPersistentStore);
-const store = createEnhancedStore(reducer, DEFAULT_STATE);
 
 const GitHub = () =>
   <svg aria-hidden="true" height="28" role="img" version="1.1" viewBox="0 0 16 16" width="28" fill="#FFFFFF">
@@ -43,6 +25,8 @@ const GitHub = () =>
 // To get it back, components must be wrapped with : connect()(Component)
 export default class extends React.Component {
   render() {
+    const store = configureStore();
+    
     return (
       <div style={STYLE_APP}>
         <h1 style={STYLE_TITLE}>Never forget your daily <a style={{verticalAlign: 'middle'}} title="https://github.com/chtefi/scrum-daily" target="_blank" href="https://github.com/chtefi/scrum-daily"><GitHub /></a></h1>
---
+++ src/reducers/store.js
@@ -0,0 +1,34 @@
+import { createStore } from 'redux'
+import reducer from '../reducers';
+//import persistState from 'redux-localstorage';
+
+//import logState from '../middlewares/logState.js';
+//import logAction from '../middlewares/logAction.js';
+//
+const DEFAULT_STATE = {
+  users: [
+    { id: 1, name: 'Oswald Cobblepot', photo: 'https://randomuser.me/api/portraits/thumb/men/92.jpg', tasks: [ { id: 1, text: 'Trying to make the state works', cdate: '2016-01-27', ddate: '2016-02-01' }, { id: 2, text: 'Trying to find which background-color fits', cdate: '2016-02-01', ddate: '2016-02-03' } ]},
+    { id: 2, name: 'Butch Gilzean', photo: 'https://randomuser.me/api/portraits/thumb/men/91.jpg', tasks: [ { id: 3, text: 'Refactor the whole project', cdate: '2016-02-01', ddate: '2016-02-03' }, { id: 4, text: 'Sending some emails', cdate: '2016-02-01', ddate: null } ]},
+    { id: 3, name: 'Fish Mooney', photo: 'https://randomuser.me/api/portraits/thumb/men/90.jpg', tasks: [ { id: 5, text: 'Do some big data', cdate: '2016-02-04', ddate: null }, { id: 6, text: 'Create a new JS framework', cdate: '2016-02-02', ddate: null } ]},
+  ],
+  weeks: {}
+};
+
+// TODO(sd): Ultra ugly for now, must create distinct files to the stores, and for the root component
+// because right now, we import the whole stuff in the production bundle : it sucks.
+// https://github.com/gaearon/redux-devtools/blob/master/docs/Walkthrough.md
+//const isProduction = (process.env.NODE_ENV === 'production');
+//const createPersistentStore = (isProduction ? compose(persistState())(createStore) : compose(persistState(), DevTools.instrument())(createStore));
+//const createEnhancedStore = storeEnhancer(createPersistentStore);
+
+
+export default function configureStore() {
+  const store = createStore(reducer, DEFAULT_STATE);
+  // store.asyncReducers = {};
+  return store;
+}
+
+// export function injectAsyncReducer(store, name, asyncReducer) {
+//   store.asyncReducers[name] = reducer;
+//   store.replaceReducer(createReducer(store.asyncReducers));
+// }
